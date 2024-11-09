6f8efd900fb4d294d70e2d2f004fa50fdd1c8f7d
DESKTOP-V2F1VDE\YogaPro <heti25@gmail.com>
2024-11-09 10:02:14 +0100

    Create New Objects From SchoolClass Class

--- SchoolClass/SchoolClassProject/Program.cs
+++ SchoolClass/SchoolClassProject/Program.cs
@@ -1,9 +1,15 @@
-﻿namespace SchoolClassProject;
+﻿using SchoolClassProject.Models;
+
+namespace SchoolClassProject;
 
 class Program
 {
     static void Main()
     {
-        
+        SchoolClass class9A = new SchoolClass(9, 'A', 70000);
+        SchoolClass class9B = new SchoolClass(9, 'b', 55000);
+
+        Console.WriteLine(class9A);
+        Console.WriteLine(class9B);
     }
 }
